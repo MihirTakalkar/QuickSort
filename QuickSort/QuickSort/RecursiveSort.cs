c661a7f7ba7f47322852505557c22bbde93c80e3
MihirTakalkar <mtakalkar0001@mymail.lausd.net>
2020-11-15 12:45:50 -0800

    Initial Commit.

---
+++ QuickSort/QuickSort/RecursiveSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickSort
+{
+    class RecursiveSort<T>
+    {
+        public List<T> Partition(List<T> list)
+        {
+
+
+            return list; 
+        }
+
+        public void Sort(List<T> list)
+        {
+
+
+        }
+    }
+}
